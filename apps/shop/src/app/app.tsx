2413b3365a71af65c6c428c01ec5cc619ce4db4d
Abhishek Singh <abhinav210702@gmail.com>
2022-07-05 16:09:12 +0530

    First Commit

---
+++ apps/shop/src/app/app.tsx
@@ -0,0 +1,15 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+// import NxWelcome from './nx-welcome';
+
+export function App() {
+  return (
+    <>
+      {/* <NxWelcome title="shop" /> */}
+      {/* <div /> */}
+      <h1 className='text-7xl text-green-500'>Hey</h1>
+    </>
+  );
+}
+
+export default App;
